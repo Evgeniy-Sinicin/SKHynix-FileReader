f27de3b2ed5c6c34345f80bcaa010d5bc3e93a08
ESinicin <catpuff13337@gmail.com>
2021-11-09 21:57:51 +0300

    Implemented project

---
+++ SKhynix.ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SKhynix.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pattern = "dotnet";
+
+            Array.ForEach(args, directoryPath =>
+            {
+                if (Directory.Exists(directoryPath))
+                {
+                    var files = Directory.GetFiles(directoryPath);
+
+                    Array.ForEach(files, file =>
+                    {
+                        Console.WriteLine($"{file} - {GetEntryCount(File.ReadAllText(file), pattern)}");
+                    });
+                }
+            });
+        }
+
+        public static int GetEntryCount(string text, string entry)
+        {
+            return text.Split(entry).Length - 1;
+        }
+    }
+}
